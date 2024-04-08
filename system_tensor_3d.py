95e46996883340783393c2e7551ef34d55083060
ohara <kzAme79@gmail.com>
2024-04-08 21:07:04 +0900

    Update system_tensor_3d.py

--- system_tensor_3d.py
+++ system_tensor_3d.py
@@ -5,7 +5,18 @@ import matplotlib.pyplot as plt
 import localConfig
 
 
-def plot_tensor(tensor, x, save_path=None):
+def plot_tensor(tensor, threshold, save_path=None):
+    """
+    Plot the 3D points of a tensor that are greater than or equal to a threshold value.
+
+    Args:
+        tensor (numpy.ndarray): The input tensor of shape (d, h, w).
+        threshold (float): The threshold value for selecting points to plot.
+        save_path (str, optional): The path to save the plot image. If None, the plot is not saved.
+
+    Returns:
+        None
+    """
     # テンソルのサイズを取得
     d, h, w = tensor.shape
 
@@ -17,7 +28,7 @@ def plot_tensor(tensor, x, save_path=None):
     for i in range(d):
         for j in range(h):
             for k in range(w):
-                if tensor[i, j, k] >= x:
+                if tensor[i, j, k] >= threshold:
                     x_coords.append(i)
                     y_coords.append(j)
                     z_coords.append(k)
@@ -29,7 +40,7 @@ def plot_tensor(tensor, x, save_path=None):
     ax.set_xlabel('i')
     ax.set_ylabel('j')
     ax.set_zlabel('l')
-    ax.set_title(f'Tensor Values >= {x}')
+    ax.set_title(f'Tensor Values >= {threshold}')
 
     ax.set_xlim(0, d)
     ax.set_ylim(0, h)
@@ -43,7 +54,7 @@ def plot_tensor(tensor, x, save_path=None):
 INDEX = 1
 
 H_tensor = np.load(localConfig.DATA_PATH + 'systemMatrix/H_matrix_tensor.npy')
-x = 0.005  # 表示する値の閾値
+THRESHOLD = 0.005  # 表示する値の閾値
 SAVE_PATH = localConfig.DATA_PATH + '240331/system_tensor_ijl_k' + str(INDEX)
 
-plot_tensor(H_tensor[:, :, INDEX, :], x, SAVE_PATH)
+plot_tensor(H_tensor[:, :, INDEX, :], THRESHOLD, SAVE_PATH)

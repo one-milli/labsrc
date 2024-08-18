79157e37a623559660133128172807f407d2341c
ohara <kzAme79@gmail.com>
2024-08-19 02:03:48 +0900

    Update change_filename.py

--- util/change_filename.py
+++ util/change_filename.py
@@ -8,7 +8,6 @@ FOLDER_PATH = DATA_PATH + "/hadamard128_input/"
 
 for filename in os.listdir(FOLDER_PATH):
     if filename.startswith("hadamard") and filename.endswith(".png"):
-        # ファイル名を分割して新しい名前を作成します。
         parts = filename.split("hadamard")
         new_filename = "hadamard_" + parts[1]
 

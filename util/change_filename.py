22c60b7546815653524718d4169044346f03a35d
ohara <kzAme79@gmail.com>
2024-08-19 01:13:41 +0900

    edit rename

--- util/change_filename.py
+++ util/change_filename.py
@@ -1,18 +1,17 @@
 """Change filename"""
 
 import os
-import localConfig
 
 # 変更を行いたいフォルダのパス
-FOLDER_PATH = localConfig.DATA_PATH + '/hadamard64_cap_R_230516_128/'
+DATA_PATH = "../data"
+FOLDER_PATH = DATA_PATH + "/hadamard128_input/"
 
 for filename in os.listdir(FOLDER_PATH):
     if filename.startswith("hadamard") and filename.endswith(".png"):
         # ファイル名を分割して新しい名前を作成します。
-        parts = filename.split('hadamard64')
-        new_filename = 'hadamard' + parts[1]
+        parts = filename.split("hadamard")
+        new_filename = "hadamard_" + parts[1]
 
-        os.rename(os.path.join(FOLDER_PATH, filename),
-                  os.path.join(FOLDER_PATH, new_filename))
+        os.rename(os.path.join(FOLDER_PATH, filename), os.path.join(FOLDER_PATH, new_filename))
 
 print("ファイル名の変更が完了しました。")

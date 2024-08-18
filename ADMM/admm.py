ca9f14bdd604978d3166d3ef417b8e9bee7405e8
ohara <kzAme79@gmail.com>
2024-08-19 04:36:34 +0900

    use cuda

--- ADMM/admm.py
+++ ADMM/admm.py
@@ -3,9 +3,9 @@ Solver for problem below using ADMM
 min_f ||g-Hf||^2 + tau*||Df||_1 + i(f)
 """
 
-import numpy as np
-import scipy.sparse as sp
-import scipy.sparse.linalg as spla
+import cupy as cp  # NumPyの代わりにCuPyをインポート
+import cupyx.scipy.sparse as csp  # SciPyのスパース行列モジュールをCuPyで置き換え
+import cupyx.scipy.sparse.linalg as cspla  # SciPyのスパース線形代数モジュールをCuPyで置き換え
 
 
 class Admm:
@@ -15,33 +15,33 @@ class Admm:
     mu3 = 1e-1
 
     def __init__(self, H, g, D, tau):
-        self.H = H  # System matrix (sparse)
-        self.g = g  # Observation
-        self.D = D  # Gradient operator (sparse)
+        self.H = csp.csr_matrix(cp.asarray(H))
+        self.g = cp.asarray(g)
+        self.D = csp.csr_matrix(cp.asarray(D))
         self.tau = tau
-        self.HTH = H.T @ H  # This will also be sparse
-        self.DTD = D.T @ D  # This will also be sparse
-        self.m, self.n = H.shape
-        self.r = np.zeros((self.n, 1))
-        self.f = np.zeros((self.n, 1))
-        self.z = np.zeros((2 * self.n, 1))
-        self.y = np.zeros((self.m, 1))
-        self.w = np.zeros((self.n, 1))
-        self.x = np.zeros((self.n, 1))
-        self.eta = self.mu2 * self.D @ self.f  # Use sparse matrix multiplication
-        self.rho = np.zeros((self.n, 1))
+        self.HTH = self.H.T @ self.H
+        self.DTD = self.D.T @ self.D
+        self.m, self.n = self.H.shape
+        self.r = cp.zeros((self.n, 1))
+        self.f = cp.zeros((self.n, 1))
+        self.z = cp.zeros((2 * self.n, 1))
+        self.y = cp.zeros((self.m, 1))
+        self.w = cp.zeros((self.n, 1))
+        self.x = cp.zeros((self.n, 1))
+        self.eta = self.mu2 * self.D @ self.f
+        self.rho = cp.zeros((self.n, 1))
         self.obj = []
         self.obj.append(self.compute_obj())
         self.err = []
 
     def soft_threshold(self, x, sigma):
-        return np.maximum(0, abs(x) - sigma) * np.sign(x)
+        return cp.maximum(0, cp.abs(x) - sigma) * cp.sign(x)
 
     def compute_obj(self):
-        return np.linalg.norm(self.g - self.H @ self.f) ** 2 + self.eta * spla.norm(self.D @ self.f, 1)
+        return cp.linalg.norm(self.g - self.H @ self.f) ** 2 + self.eta * cspla.norm(self.D @ self.f, 1)
 
     def compute_err(self, f):
-        return np.linalg.norm(f - self.f)
+        return cp.linalg.norm(f - self.f)
 
     def update_r(self):
         self.r = (
@@ -52,13 +52,13 @@ class Admm:
         )
 
     def update_f(self):
-        self.f = spla.spsolve(self.HTH + self.mu2 * self.DTD + self.mu3 * sp.eye(self.m, self.m), self.r)
+        self.f = cspla.spsolve(self.HTH + self.mu2 * self.DTD + self.mu3 * csp.eye(self.m, self.m), self.r)
 
     def update_z(self):
         self.z = self.soft_threshold(self.D @ self.f + self.eta / self.mu2, self.tau / self.mu2)
 
     def update_w(self):
-        self.w = np.clip(self.f + self.rho / self.mu3, 0, 1)
+        self.w = cp.clip(self.f + self.rho / self.mu3, 0, 1)
 
     def update_y(self):
         self.y = self.H @ self.f + self.g
@@ -89,4 +89,4 @@ class Admm:
             if error < 1e-4:
                 break
             print("iter =", i, "obj =", self.obj[-1], "err =", self.err[-1])
-        return self.f, self.obj, self.err
+        return cp.asnumpy(self.f), cp.asnumpy(self.obj), cp.asnumpy(self.err)

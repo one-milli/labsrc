5cdc4637811f1abd779cb04b754939753ec21be9
ohara <kzAme79@gmail.com>
2024-08-25 13:20:09 +0900

    Update admm.py

--- ADMM/admm.py
+++ ADMM/admm.py
@@ -3,14 +3,14 @@ Solver for problem below using ADMM
 min_f ||g-Hf||^2 + tau*||Df||_1 + i(f)
 """
 
-import cupy as cp  # NumPyの代わりにCuPyをインポート
+import cupy as cp
 
 
 class Admm:
     max_iter = 500
-    mu1 = 1e1
-    mu2 = 1e-1
-    mu3 = 1e-1
+    mu1 = 1e-5
+    mu2 = 1e-4
+    mu3 = 1e-4
 
     def __init__(self, H, g, D, tau):
         self.H = cp.asarray(H)
@@ -21,22 +21,20 @@ class Admm:
         self.DTD = self.D.T @ self.D
         self.m, self.n = self.H.shape
         self.r = cp.zeros((self.n, 1))
-        self.f = cp.zeros((self.n, 1))
-        self.z = cp.zeros((2 * self.n, 1))
+        self.f = cp.ones((self.n, 1)) / 2
+        self.z = cp.zeros((self.D.shape[0], 1))
         self.y = cp.zeros((self.m, 1))
         self.w = cp.zeros((self.n, 1))
-        self.x = cp.zeros((self.n, 1))
+        self.x = cp.zeros((self.m, 1))
         self.eta = self.mu2 * self.D @ self.f
         self.rho = cp.zeros((self.n, 1))
-        self.obj = []
-        self.obj.append(self.compute_obj())
         self.err = []
 
     def soft_threshold(self, x, sigma):
         return cp.maximum(0, cp.abs(x) - sigma) * cp.sign(x)
 
     def compute_obj(self):
-        return cp.linalg.norm(self.g - self.H @ self.f) ** 2 + self.eta * cp.linalg.norm(self.D @ self.f, 1)
+        return cp.linalg.norm(self.g - self.H @ self.f) ** 2 + self.tau * cp.linalg.norm(self.D @ self.f, 1)
 
     def compute_err(self, f):
         return cp.linalg.norm(f - self.f)
@@ -50,7 +48,8 @@ class Admm:
         )
 
     def update_f(self):
-        self.f = cp.linalg.inv(self.HTH + self.mu2 * self.DTD + self.mu3 * cp.eye(self.m, self.m)).dot(self.r)
+        A = self.HTH + self.mu2 * self.DTD + self.mu3 * cp.eye(self.n)
+        self.f = cp.linalg.solve(A, self.r)
 
     def update_z(self):
         self.z = self.soft_threshold(self.D @ self.f + self.eta / self.mu2, self.tau / self.mu2)
@@ -72,7 +71,7 @@ class Admm:
 
     def solve(self):
         for i in range(self.max_iter):
-            f = self.f
+            pre_f = self.f.copy()
             self.update_r()
             self.update_f()
             self.update_z()
@@ -81,10 +80,13 @@ class Admm:
             self.update_x()
             self.update_eta()
             self.update_rho()
-            self.obj.append(self.compute_obj())
-            error = self.compute_err(f)
+            error = self.compute_err(pre_f)
+            if i > 0:
+                err_diff = abs(error - self.err[-1])
+            else:
+                err_diff = error
             self.err.append(error)
-            if error < 1e-4:
+            print("iter =", i, "err =", error, "diff =", err_diff)
+            if err_diff < 1e-5:
                 break
-            print("iter =", i, "obj =", self.obj[-1], "err =", self.err[-1])
-        return cp.asnumpy(self.f), cp.asnumpy(self.obj), cp.asnumpy(self.err)
+        return cp.asnumpy(self.f), cp.asnumpy(self.err)

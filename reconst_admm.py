cac68f8bf4a4757a6f0f4d054056dfd6935b301b
ohara <kzAme79@gmail.com>
2024-08-25 15:38:15 +0900

    update reconst_admm

--- reconst_admm.py
+++ reconst_admm.py
@@ -1,4 +1,4 @@
- %%
+# %%
 import numpy as np
 import scipy.sparse as sps
 import scipy.io as sio
@@ -7,12 +7,14 @@ import admm
 
 # %%
 # DATA_PATH = '../../../OneDrive - m.titech.ac.jp/Lab/data'
-DATA_PATH = '../data'
+DATA_PATH = "../data"
 OBJ_NAME = "Text"
 H_SETTING = "hadamard_FISTA_l122_p-10_lmd-100"
+# H_SETTING = "gf"
 n = 128
 m = 256
 tau = 1e1
+
 
 # %%
 def createDrgb(n):
@@ -55,8 +57,8 @@ g = captured.reshape(-1, 1)
 
 # %%
 # H = np.load(f"{DATA_PATH}/systemMatrix/H_matrix_{SETTING}.npy")
-H = sio.mmread(f"{DATA_PATH}/240818/systemMatrix/H_sparse_{H_SETTING}.mtx").tocsr()
-print(sio.mminfo(f"{DATA_PATH}/240818/systemMatrix/H_sparse_{H_SETTING}.mtx"))
+H = sio.mmread(f"{DATA_PATH}/240825/systemMatrix/H_sparse_{H_SETTING}.mtx").tocsr()
+print(sio.mminfo(f"{DATA_PATH}/240825/systemMatrix/H_sparse_{H_SETTING}.mtx"))
 
 # %%
 H = H.toarray()
@@ -71,7 +73,5 @@ f = np.clip(f, 0, 1) * 255
 F = f.reshape(n, n)
 F = F.astype(np.uint8)
 F_image = Image.fromarray(F)
-F_image.save(f"{DATA_PATH}/240818/reconst/{OBJ_NAME}_admm_t-{tau}.png")
-print(f"Saved {DATA_PATH}/240818/reconst/{OBJ_NAME}_admm_t-{tau}.png")
-
-
+F_image.save(f"{DATA_PATH}/240825/reconst/{OBJ_NAME}_admm_t-{tau}.png")
+print(f"Saved {DATA_PATH}/240825/reconst/{OBJ_NAME}_admm_t-{tau}.png")

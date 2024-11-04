1d23b8c175366136e883ef481409e4a7e812609a
one-milli <kzAme79@gmail.com>
2024-11-04 15:17:35 +0900

    upd

--- compare_h.py
+++ compare_h.py
@@ -1,19 +1,15 @@
 import numpy as np
 
 
-def tensor_to_matrix(tensor, m, n):
-    return tensor.reshape(m * m, n * n)
-
-
 DATA_PATH = "../data"
 
 H_SETTING = "hadamard_FISTA_p-5_lmd-1_m-255"
-H = np.load(f"{DATA_PATH}/241103/systemMatrix/H_matrix_{H_SETTING}.npy")
+H1 = np.load(f"{DATA_PATH}/241103/systemMatrix/H_matrix_{H_SETTING}.npy")
+print(f"H1: {H1.shape}")
 
 H_SETTING = "hadamard_FISTA_p-5_lmd-1_m-128"
-tensor = np.load(f"{DATA_PATH}/241103/systemMatrix/H_tensor_int_{H_SETTING}.npy")
-print(f"tensor shape: {tensor.shape}")
-interpolated_H = tensor_to_matrix(tensor, 255, 128)
+H2 = np.load(f"{DATA_PATH}/241103/systemMatrix/H_matrix_int_{H_SETTING}.npy")
+print(f"H2: {H2.shape}")
 
-rem = np.linalg.norm(H - interpolated_H)
+rem = np.linalg.norm(H1 - H2)
 print(f"rem: {rem}")

aaeaacc2d5f1b32ac5409a974490dcc587427129
Kazuma Ohara <kzAme79@gmail.com>
2024-11-07 16:50:34 +0900

    Update processCapturedImage.m

--- common_func_mat/processCapturedImage.m
+++ common_func_mat/processCapturedImage.m
@@ -1,10 +1,9 @@
 function processedImg = processCapturedImage(capturedImg, params)
     % Trim and resize the captured image
     try
-        % show shape of capturedImg
-        disp(size(capturedImg));
         trimmedImg = capturedImg(params.trimRowFrom:params.trimRowTo, ...
             params.trimColFrom:params.trimColTo);
+        disp(size(trimmedImg));
         processedImg = uint8(imresize(trimmedImg, [params.m, params.m]));
     catch ME
         error('Error processing captured image: %s', ME.message);

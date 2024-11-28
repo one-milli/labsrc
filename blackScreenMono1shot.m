e6cf317212c6084d7b97a57db38a07b8273fd817
Kazuma Ohara <kzAme79@gmail.com>
2024-11-28 15:31:57 +0900

    256upd

--- blackScreenMono1shot.m
+++ blackScreenMono1shot.m
@@ -6,8 +6,8 @@ trimRowFrom = 351;
 trimRowTo = 850;
 trimColFrom = 351;
 trimColTo = 850;
-m = 255;
-n = 128;
+m = 511;
+n = 256;
 nn = n * n;
 
 pause('on')
@@ -19,8 +19,8 @@ wy_pro = 1200;
 wx_pc = 1366;
 wy_pc = 768;
 %Camera resolution
-wx_cam = 1280;
-wy_cam = 1024;
+% wx_cam = 1280;
+% wy_cam = 1024;
 
 Nnumx = round(log2(wx_pro)); %=11
 Nnumy = round(log2(wy_pro)) + 1; %=10+1 to cover all the pixels
@@ -39,7 +39,7 @@ ha = axes('Parent', h, 'Units', 'pixels', 'Position', [1 1 wx_pro wy_pro]);
 Cam_mode = 'F7_Mono8_1288x964_Mode0';
 vid = videoinput('pointgrey', 1, Cam_mode);
 src = getselectedsource(vid);
-mg = 2; %magnification
+mg = 1; %magnification
 ulc = 500;
 ulr = 750;
 vid.FramesPerTrigger = 1;
@@ -49,7 +49,7 @@ start(vid);
 
 %% capture
 filename = 'Text';
-input = uint8(imread(['../../OneDrive - m.titech.ac.jp/Lab/data/sample_image256/', filename, '.png']));
+input = uint8(imread(['../../OneDrive - m.titech.ac.jp/Lab/data/sample_image', int2str(n), '/', filename, '.png']));
 input = imresize(input, [n, n]);
 image_disp = input;
 
@@ -70,12 +70,12 @@ imshow(uint8(Line), 'Parent', ha);
 
 pause(2)
 
-% trigger(vid);
-% img = getdata(vid, 1);
-% img = img(trimRowFrom:trimRowTo, trimColFrom:trimColTo);
-% img = imresize(img, [m m]);
+trigger(vid);
+img = getdata(vid, 1);
+img = img(trimRowFrom:trimRowTo, trimColFrom:trimColTo);
+img = imresize(img, [m m]);
 
-% imwrite(img, ['../../OneDrive - m.titech.ac.jp/Lab/data/capture_', expDate, '/', filename, '.png'], 'BitDepth', 8);
+imwrite(img, ['../../OneDrive - m.titech.ac.jp/Lab/data/capture_', expDate, '/', filename, '.png'], 'BitDepth', 8);
 
 %% Stop camera
 stop(vid);

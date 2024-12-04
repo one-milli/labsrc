aaef5d8e5033d78b8e371f75dd80290db57ef149
ohara <kzAme79@gmail.com>
2024-12-04 12:55:26 +0900

    upd

--- compare_h.py
+++ compare_h.py
@@ -2,13 +2,14 @@ import cupy as cp
 
 DATA_PATH = "../data"
 
-H1_SETTING = "hadamard_FISTA_p-5_lmd-1_m-255"
-H1 = cp.load(f"{DATA_PATH}/241103/systemMatrix/H_matrix_{H1_SETTING}.npy")
+H1_SETTING = "gf"
+H1 = cp.load(f"{DATA_PATH}/241127/systemMatrix/H_matrix_{H1_SETTING}.npy")
 print(f"H1: {H1.shape}")
 
-H2_SETTING = "hadamard_FISTA_p-5_lmd-1_m-128"
-H2 = cp.load(f"{DATA_PATH}/241103/systemMatrix/H_matrix_int_{H2_SETTING}.npy")
-print(f"H2: {H2.shape}")
+H2_SETTING = "int_p-5_lmd-100_to-True"
+# H2 = cp.load(f"{DATA_PATH}/241127/systemMatrix/H_matrix_{H2_SETTING}.npy")
+# print(f"H2: {H2.shape}")
 
+H2=cp.zeros(H1.shape)
 rem = cp.linalg.norm(H1 - H2)
 print(f"rem: {rem}")

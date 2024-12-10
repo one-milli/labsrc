c71b6d8c76bedd52f9274137450e462d25bbb9c7
ohara <kzAme79@gmail.com>
2024-12-11 03:57:44 +0900

    test

--- estimate_h_fista2.py
+++ estimate_h_fista2.py
@@ -1,47 +1,90 @@
 # %%
 import os
-import time
-from typing import Callable
-import cupy as cp
+import math
 import numpy as np
-from cupyx.scipy import sparse as cps
+import cupy as cp
+import cupyx.scipy.sparse as csp
+import cupyx.scipy.sparse.linalg as csla
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from PIL import Image
+from IPython.display import display
 import package.myUtil as myUtil
 
 # %%
+cap_dates = {128: "241114", 256: "241205"}
 n = 128
-m = 128
-N = n**2
-M = m**2
-LAMBDA = 1
+LAMBDA = 100
 RATIO = 0.05
-# DATA_PATH = "../../OneDrive - m.titech.ac.jp/Lab/data"
+DO_THIN_OUT = False
+SAVE_AS_SPARSE = True
 DATA_PATH = "../data"
 IMG_NAME = "hadamard"
-DIRECTORY = DATA_PATH + "/241022"
-SETTING = f"{IMG_NAME}_FISTA_p-{int(100*RATIO)}_lmd-{LAMBDA}_m-{m}"
+CAP_DATE = cap_dates[n]
+EXP_DATE = "241206"
+DIRECTORY = f"{DATA_PATH}/{EXP_DATE}"
+SETTING = f"p-{int(100*RATIO)}_lmd-{LAMBDA}"
+if DO_THIN_OUT:
+    SETTING = SETTING + "to"
 
 if not os.path.exists(DIRECTORY):
     os.makedirs(DIRECTORY)
 if not os.path.exists(DIRECTORY + "/systemMatrix"):
     os.makedirs(DIRECTORY + "/systemMatrix")
+use_list = myUtil.get_use_list(n * n, RATIO)
+
+# %%
+G = myUtil.images2matrix(f"{DATA_PATH}/{IMG_NAME}{n}_cap_{CAP_DATE}/", use_list, thin_out=DO_THIN_OUT)
+F = myUtil.images2matrix(f"{DATA_PATH}/{IMG_NAME}{n}_input/", use_list).astype(cp.int8)
+M, K = G.shape
+N, K = F.shape
+print("G shape:", G.shape, "F shape:", F.shape, "M=", M, "N=", N, "K=", K)
+print("G max:", G.max(), "G min:", G.min(), "F max:", F.max(), "F min:", F.min())
+
+black = myUtil.calculate_bias(M, DATA_PATH, CAP_DATE)
+B = cp.tile(black[:, None], K)
+
+G = G - B
+
+white_img = Image.open(f"{DATA_PATH}/capture_{CAP_DATE}/White.png").convert("L")
+white = (cp.asarray(white_img) / 255).astype(cp.float32)
+if DO_THIN_OUT:
+    white = white[::2, ::2].ravel() - black
+else:
+    white = white.ravel() - black
+H1 = cp.tile(white[:, None], K)
+
+F_hat = 2 * F - 1
+G_hat = 2 * G - H1
+del F, G, H1
+cp._default_memory_pool.free_all_blocks()
+
+# %%
+fft = (F_hat @ F_hat.T).astype(cp.int8)
+fgt = (F_hat @ G_hat.T).astype(cp.float32)
+del F_hat, G_hat
+cp._default_memory_pool.free_all_blocks()
 
 
 # %%
-def prox_l122(Y: cp.ndarray, gamma: float) -> cps.csr_matrix:
-    l1_norms = cp.sum(cp.absolute(Y), axis=1)
+def prox_l1(Y: cp.ndarray, gamma: float) -> cp.ndarray:
+    return csp.csr_matrix(cp.sign(Y) * cp.maximum(cp.absolute(Y) - gamma, 0))
+
+
+def prox_l122(Y: cp.ndarray, gamma: float, N: int) -> csp.csr_matrix:
     factor = (2 * gamma) / (1 + 2 * gamma * N)
+    l1_norms = cp.sum(cp.absolute(Y), axis=1)
     X = cp.sign(Y) * cp.maximum(cp.absolute(Y) - factor * l1_norms[:, None], 0)
-    # return cps.csr_matrix(X)
-    return X
+    return csp.csr_matrix(X)
 
 
 def fista(
-    Ft: cp.ndarray,
-    Gt: cp.ndarray,
+    fft: cp.ndarray,
+    fgt: cp.ndarray,
     lmd: float,
-    prox: Callable[[cp.ndarray, float], cp.ndarray],
+    N: int,
+    M: int,
+    chunk_size: int,
     max_iter: int = 500,
     tol: float = 1e-3,
 ) -> cp.ndarray:
@@ -57,91 +100,74 @@ def fista(
     Returns:
     - h: numpy array, the solution vector h
     """
-    N = Ft.shape[1]
-    M = Gt.shape[1]
     t = 1
-    Ht = cp.zeros((N, M), dtype=cp.float32)
-    Ht_old = cp.zeros_like(Ht)
-    Yt = cp.zeros_like(Ht)
-    # Ht = cps.csr_matrix((N, M), dtype=cp.float32)
-    # Ht_old = cps.csr_matrix((N, M), dtype=cp.float32)
-    # Yt = cps.csr_matrix((N, M), dtype=cp.float32)
-    ff = Ft.T @ Ft
+    Ht = csp.csr_matrix((N, M), dtype=cp.float32)
+    Ht_old = csp.csr_matrix((N, M), dtype=cp.float32)
+    Yt = csp.csr_matrix((N, M), dtype=cp.float32)
 
     # Lipschitz constant
     # L = np.linalg.norm(Ft.T @ Ft, ord=2) * 3
     gamma = 1 / (N * 3)
-    # H_true = cp.load(f"{DIRECTORY}/systemMatrix/H_matrix_hadamard_gf.npy")
 
-    # start = time.perf_counter()
     for i in range(max_iter):
         t_old = t
         Ht_old = Ht.copy()
 
-        # A_nonsparse = ff @ Ht
-        # A_nonsparse = Yt - gamma * A_nonsparse
-        # A_nonsparse = A_nonsparse - Ft.T @ Gt
-        # Ht = prox(A_nonsparse, gamma * lmd)
-        Ht = prox(Yt - gamma * ff @ Ht - Ft.T @ Gt, gamma * lmd)
+        for c in range(32):
+            start = c * chunk_size
+            end = min((c + 1) * chunk_size, M)
+            A_dense = Yt[:, start:end] - gamma * (fft @ Yt[:, start:end] - fgt[:, start:end])
+            Ht[:, start:end] = prox_l1(A_dense, gamma * lmd)
         t = (1 + np.sqrt(1 + 4 * t_old**2)) / 2
         Yt = Ht + ((t_old - 1) / t) * (Ht - Ht_old)
 
-        error = cp.linalg.norm(Ht - Ht_old) / cp.linalg.norm(Ht)
-        # error = cps.linalg.norm(Ht - Ht_old) / cps.linalg.norm(Ht)
+        error = csla.norm(Ht - Ht_old) / csla.norm(Ht)
         print(f"iter: {i}, error: {error}")
-        # rem = cp.linalg.norm(Ht - H_true.T)
-        # print(f"iter: {i}, error: {error}, rem: {rem}")
         if error < tol:
             break
-
-    # end = time.perf_counter()
-    # print(f"Elapsed time: {end-start}")
 
     return Ht
 
 
 # %%
-# load images
-# INFO = "cap_R_230516_128"
-INFO = "cap_240814"
-G, _ = myUtil.images_to_matrix(f"{DATA_PATH}/{IMG_NAME}{n}_{INFO}/", ratio=RATIO, resize=True, ressize=m)
-F, _ = myUtil.images_to_matrix(f"{DATA_PATH}/{IMG_NAME}{n}_input/", ratio=RATIO)
-K = F.shape[1]
-print("K=", K)
-white_img = Image.open(f"{DATA_PATH}/{IMG_NAME}{n}_{INFO}/{IMG_NAME}_1.png").convert("L")
-white_img = white_img.resize((m, m))
-white = np.asarray(white_img).ravel() / 255
-white = white[:, np.newaxis]
-H1 = np.tile(white, F.shape[1])
-F_hat = 2 * F - 1
-G_hat = 2 * G - H1
-
-# %%
-F_hat_T_gpu = cp.asarray(F_hat.T).astype(cp.int8)
-G_hat_T_gpu = cp.asarray(G_hat.T).astype(cp.float32)
-del F, G, H1, F_hat, G_hat
-
-# %%
-Ht = fista(F_hat_T_gpu, G_hat_T_gpu, LAMBDA, prox_l122)
-
-# %%
+chunk_size = M // 32
+Ht = fista(fft, fgt, LAMBDA, N, M, chunk_size)
 H = Ht.T
 del Ht
-np.save(f"{DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npy", H)
-print(f"Saved {DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npy")
 
+# %%
+if SAVE_AS_SPARSE:
+    print(f"shape: {H.shape}, nnz: {H.nnz}({H.nnz / H.shape[0] / H.shape[1] * 100:.2f}%)")
+    H_np = {
+        "data": cp.asnumpy(H.data),
+        "indices": cp.asnumpy(H.indices),
+        "indptr": cp.asnumpy(H.indptr),
+        "shape": H.shape,
+    }
+    np.savez(f"{DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npz", **H_np)
+    print(f"Saved {DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npz")
+    # myUtil.plot_sparse_matrix_cupy(H, row_range=(5500, 6000), col_range=(4500, 5000), markersize=1)
+else:
+    cp.save(f"{DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npy", H)
+    print(f"Saved {DIRECTORY}/systemMatrix/H_matrix_{SETTING}.npy")
+
+# %%
 SAMPLE_NAME = "Cameraman"
 sample_image = Image.open(f"{DATA_PATH}/sample_image{n}/{SAMPLE_NAME}.png").convert("L")
 sample_image = cp.asarray(sample_image).flatten() / 255
 
-Hf = H @ sample_image
-Hf_img = cp.asnumpy(Hf.reshape(m, m))
-Hf_img = np.clip(Hf_img, 0, 1)
-Hf_pil = Image.fromarray((Hf_img * 255).astype(np.uint8), mode="L")
-
+m = int(math.sqrt(M))
 FILENAME = f"{SAMPLE_NAME}_{SETTING}.png"
-fig, ax = plt.subplots(figsize=Hf_img.shape[::-1], dpi=1, tight_layout=True)
-ax.imshow(Hf_pil, cmap="gray")
-ax.axis("off")
-fig.savefig(f"{DIRECTORY}/{FILENAME}", dpi=1)
+Hf = H @ sample_image + black
+Hf = cp.asnumpy(Hf.reshape(m, m))
+print("Hf shape:", Hf.shape)
+
+Hf_pil = Image.fromarray((Hf * 255).astype(np.uint8), mode="L")
+Hf_pil.save(f"{DIRECTORY}/{FILENAME}", format="PNG")
 print(f"Saved {DIRECTORY}/{FILENAME}")
+display(Hf_pil)
+
+plt.imshow(Hf, cmap="gray", interpolation="nearest")
+plt.colorbar()
+plt.title("Grayscale Heatmap")
+plt.show()
